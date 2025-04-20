74a7660b820820b4c95f880d5508e8f831d062ed
ase3238 <ase3238@gmail.com>
2025-04-20 12:14:16 +0900

    style(view): showRoundMsg 함수명 변경

--- src/main/kotlin/view/RaceView.kt
+++ src/main/kotlin/view/RaceView.kt
@@ -5,7 +5,7 @@ class RaceView {
         println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     }
 
-    fun showRoundMsg() {
+    fun showRoundInitMsg() {
         println("시도할 회수는 몇회인가요?")
     }
 
